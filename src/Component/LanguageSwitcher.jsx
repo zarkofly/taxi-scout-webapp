e2abb2cbdfcfe4028e83f5d6ebce25d8caa0dc4d
zarkofly <fly21882@gmail.com>
2024-11-04 19:51:42 +0100

    Update LanguageSwitcher.jsx

--- src/Component/LanguageSwitcher.jsx
+++ src/Component/LanguageSwitcher.jsx
@@ -15,8 +15,8 @@ const LanguageSwitcher = () => {
 
 
       <option value ='en'>English</option>
-      <option value ='es'>Español</option>
            <option value ='ge'>German</option>
+                <option value ='es'>Español</option>
         </select>
     </div>
   );
